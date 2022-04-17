dbd01a3ed3c0c77d502f77421134938c05618f9e
bvegaM <vegabryam40@gmail.com>
2022-04-17 11:44:32 -0500

    declaracion de beans de forma explicita

--- spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/DependencyInyectionApplication.java
+++ spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/DependencyInyectionApplication.java
@@ -1,27 +1,27 @@
 package com.example.spring.core.dependencyinyection;
 
-import com.example.spring.core.dependencyinyection.atr.Coche;
-import com.example.spring.core.dependencyinyection.atr.Motor;
-import com.example.spring.core.dependencyinyection.profiles.EnviromentService;
-import com.example.spring.core.dependencyinyection.qualifiers.*;
 import com.example.spring.core.dependencyinyection.scopes.EjemploScopeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
 
+    @Bean
+    public String getAplicationName(){
+        return "Bryam Vega: Beans de anotacion explicita";
+    }
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
-        EjemploScopeService ejemploScopeService = context.getBean(EjemploScopeService.class);
-        EjemploScopeService ejemploScopeService2 = context.getBean(EjemploScopeService.class);
-        log.info("EjemploScopeService: {}", ejemploScopeService.equals(ejemploScopeService2));
-        log.info("EjemploScopeService: {}", ejemploScopeService == ejemploScopeService2);
+        String nombreAplicacion = context.getBean(String.class);
+        log.info("Nombre de la aplicación: {}", nombreAplicacion);
     }
 
 }
