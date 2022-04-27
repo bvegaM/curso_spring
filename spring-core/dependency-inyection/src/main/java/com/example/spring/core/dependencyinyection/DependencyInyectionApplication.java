e3e37ff47db66af02f646b5aa4e3eea8428d9ebe
bvegaM <vegabryam40@gmail.com>
2022-04-26 19:56:25 -0500

    InitializingBean and DisposableBean

--- spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/DependencyInyectionApplication.java
+++ spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/DependencyInyectionApplication.java
@@ -14,10 +14,10 @@ public class DependencyInyectionApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
 
-    @Bean(initMethod = "init", destroyMethod = "destroy")
-    public ExplicitBean getBean(){
-        return new ExplicitBean();
-    }
+    //@Bean(initMethod = "init", destroyMethod = "destroy")
+    //public ExplicitBean getBean(){
+    //    return new ExplicitBean();
+    //}
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
--- spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/lifecycle/LifeCycleBean.java
+++ spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/lifecycle/LifeCycleBean.java
@@ -3,6 +3,8 @@ package com.example.spring.core.dependencyinyection.lifecycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +12,12 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Component
-@Scope("prototype")
-public class LifeCycleBean implements BeanNameAware {
+//@Scope("prototype") -- recordemos de pre destroy no se ejecuta para beans de tipo prototype
+public class LifeCycleBean implements BeanNameAware, InitializingBean, DisposableBean {
 
     private static final Logger log = LoggerFactory.getLogger(LifeCycleBean.class);
 
+    // se ejecuta durante la construccion del bean "BeanNameAware"
     @Override
     public void setBeanName(String s) {
         log.info("Bean name is {}", s);
@@ -34,7 +37,18 @@ public class LifeCycleBean implements BeanNameAware {
     * Solo se ejecutan durante una terminación normal de la JVM
     */
     @PreDestroy
-    public void destroy(){
+    public void destroyBean(){
         log.info("Bean is destroyed");
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        log.info("Destroy method");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("After properties set method");
     }
 }
