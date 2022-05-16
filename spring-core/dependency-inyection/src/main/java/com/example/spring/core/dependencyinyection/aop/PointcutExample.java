1f34899f62043a2ce72faa6047d84ad3108de864
bvegaM <vegabryam40@gmail.com>
2022-05-16 14:18:31 -0500

    Type signature patterns

--- spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/aop/PointcutExample.java
+++ spring-core/dependency-inyection/src/main/java/com/example/spring/core/dependencyinyection/aop/PointcutExample.java
@@ -4,7 +4,8 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointcutExample {
 
-    @Pointcut("execution(* com.example.spring.core.dependencyinyection.aop.TargetObject.*(..))")
+    //@Pointcut("execution(* com.example.spring.core.dependencyinyection.aop.TargetObject.*(..))")
+    @Pointcut("@annotation(Devs4jCache)")
     public void targetMethod() {
     }
 }
